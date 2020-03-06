9e680a0a1f4391fd55a7c5fe1604ad83300a320f
wangqingang <qingang@cassianetworks.com>
2020-03-06 13:28:39 +0800

    optimiz: webpack config and add v-cloak loading

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,35 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
   output: {
-      path: path.resolve(__dirname, 'src/js'),
-    filename: 'bundle.js'
-  }
-};
+    path: path.resolve(__dirname, './dist'),
+    filename: 'js/index.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist'], {
+      root: path.resolve(__dirname, '.'),
+      dry: false
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: './src/index.html',
+        to: 'index.html'
+      },
+      {
+        from: './src/css',
+        to: 'css'
+      },
+      {
+        from: './src/img',
+        to: 'img'
+      },
+      {
+        from: './src/lib',
+        to: 'lib'
+      }
+    ])
+  ]
+}
